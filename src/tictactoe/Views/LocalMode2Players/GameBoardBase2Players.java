ced5f3a00ac2a328ed69e0880f0b130379a25950
Sarah Talaat <sarahtalaatammar.work@gmail.com>
2023-12-31 03:53:12 +0200

    Make some changes to the colors and texts on the gameboard in the local mode of the 2 players

--- src/tictactoe/Views/LocalMode2Players/GameBoardBase2Players.java
+++ src/tictactoe/Views/LocalMode2Players/GameBoardBase2Players.java
@@ -5,20 +5,31 @@ import java.awt.event.ActionListener;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.animation.RotateTransition;
+import javafx.animation.TranslateTransition;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.PerspectiveCamera;
 import javafx.scene.control.Button;
+import javafx.scene.effect.InnerShadow;
+import javafx.scene.effect.Reflection;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.transform.Rotate;
+import javafx.util.Duration;
 import tictactoe.TicTacToe;
 
 public class GameBoardBase2Players extends AnchorPane implements ActionListener{
@@ -57,7 +68,7 @@ public class GameBoardBase2Players extends AnchorPane implements ActionListener{
     protected Text player2ScoreText;
     protected final Text player2Text;
     protected final Button newGameButton;
-    protected final Button exitButton;
+    protected final Button mainMenuButton;
     protected final Text playerTurnText;
     boolean newGameFlag = true;
     // Create an array to store buttons
@@ -107,7 +118,7 @@ public class GameBoardBase2Players extends AnchorPane implements ActionListener{
         player2ScoreText = new Text();
         player2Text = new Text();
         newGameButton = new Button();
-        exitButton = new Button();
+        mainMenuButton = new Button();
         playerTurnText = new Text();
         xWinCounter=0;
         oWinCounter=0;
@@ -181,57 +192,75 @@ public class GameBoardBase2Players extends AnchorPane implements ActionListener{
         button0.setMnemonicParsing(false);
         button0.setPrefHeight(130.0);
         button0.setPrefWidth(211.0);
+        button0.setFont(new Font(50)); 
+        button0.setStyle("-fx-text-stroke: white;");
 
         GridPane.setColumnIndex(button1, 1);
         button1.setMnemonicParsing(false);
         button1.setPrefHeight(200.0);
         button1.setPrefWidth(255.0);
+        button1.setFont(new Font(50)); 
+        button1.setStyle("-fx-text-stroke: white;");
 
         GridPane.setColumnIndex(button2, 2);
         button2.setMnemonicParsing(false);
         button2.setPrefHeight(144.0);
         button2.setPrefWidth(215.0);
-
+        button2.setFont(new Font(50)); 
+        button2.setStyle("-fx-text-stroke: white;");
+        
         GridPane.setRowIndex(button3, 1);
         button3.setMnemonicParsing(false);
         button3.setPrefHeight(123.0);
         button3.setPrefWidth(212.0);
-
+        button3.setFont(new Font(50)); 
+        button3.setStyle("-fx-text-stroke: white;");
+        
         GridPane.setColumnIndex(button4, 1);
         GridPane.setRowIndex(button4, 1);
         button4.setMnemonicParsing(false);
         button4.setPrefHeight(139.0);
         button4.setPrefWidth(260.0);
-
+        button4.setFont(new Font(50)); 
+        button4.setStyle("-fx-text-stroke: white;");
+        
         GridPane.setColumnIndex(button5, 2);
         GridPane.setRowIndex(button5, 1);
         button5.setMnemonicParsing(false);
         button5.setPrefHeight(139.0);
         button5.setPrefWidth(242.0);
-
+        button5.setFont(new Font(50)); 
+        button5.setStyle("-fx-text-stroke: white;");
+        
         GridPane.setRowIndex(button6, 2);
         button6.setMnemonicParsing(false);
         button6.setPrefHeight(127.0);
         button6.setPrefWidth(234.0);
-
+        button6.setFont(new Font(50)); 
+        button6.setStyle("-fx-text-stroke: white;");
+        
         GridPane.setColumnIndex(button7, 1);
         GridPane.setRowIndex(button7, 2);
         button7.setMnemonicParsing(false);
         button7.setPrefHeight(196.0);
         button7.setPrefWidth(192.0);
-
+        button7.setFont(new Font(50)); 
+        button7.setStyle("-fx-text-stroke: white;");
+        
         GridPane.setColumnIndex(button8, 2);
         GridPane.setRowIndex(button8, 2);
         button8.setMnemonicParsing(false);
         button8.setPrefHeight(161.0);
         button8.setPrefWidth(220.0);
-
+        button8.setFont(new Font(50)); 
+        button8.setStyle("-fx-text-stroke: white;");
+        
         scoreGrid.setHgap(10.0);
         scoreGrid.setLayoutX(276.0);
         scoreGrid.setLayoutY(43.0);
         scoreGrid.setPrefHeight(99.0);
         scoreGrid.setPrefWidth(448.0);
-
+ 
         columnConstraints2.setHgrow(javafx.scene.layout.Priority.SOMETIMES);
         columnConstraints2.setMinWidth(10.0);
         columnConstraints2.setPrefWidth(100.0);
@@ -250,13 +279,13 @@ public class GameBoardBase2Players extends AnchorPane implements ActionListener{
 
         score1Pane.setPrefHeight(200.0);
         score1Pane.setPrefWidth(143.0);
-        score1Pane.setStyle("-fx-background-radius: 15; -fx-background-color: red;");
+        score1Pane.setStyle("-fx-background-radius: 15; -fx-background-color: white;");
 
         BorderPane.setAlignment(player1Text, javafx.geometry.Pos.CENTER);
         player1Text.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
         player1Text.setStrokeWidth(0.0);
         player1Text.setText("Player 1");
-        player1Text.setFont(new Font("Arial Bold", 18.0));
+        player1Text.setFont(new Font("Arial Bold", 27.0));
         BorderPane.setMargin(player1Text, new Insets(5.0, 0.0, 0.0, 0.0));
         score1Pane.setTop(player1Text);
 
@@ -270,8 +299,10 @@ public class GameBoardBase2Players extends AnchorPane implements ActionListener{
         GridPane.setColumnIndex(drawPane, 1);
         drawPane.setPrefHeight(200.0);
         drawPane.setPrefWidth(200.0);
-        drawPane.setStyle("-fx-background-radius: 15; -fx-background-color: yellow;");
-
+        drawPane.setStyle("-fx-background-radius: 15; -fx-background-color: white;");
+        
+        
+        
         BorderPane.setAlignment(drawScoreText, javafx.geometry.Pos.CENTER);
         drawScoreText.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
         drawScoreText.setStrokeWidth(0.0);
@@ -283,14 +314,14 @@ public class GameBoardBase2Players extends AnchorPane implements ActionListener{
         drawText.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
         drawText.setStrokeWidth(0.0);
         drawText.setText("Draw");
-        drawText.setFont(new Font("Arial Bold", 18.0));
+        drawText.setFont(new Font("Arial Bold", 27.0));
         BorderPane.setMargin(drawText, new Insets(5.0, 0.0, 0.0, 0.0));
         drawPane.setTop(drawText);
 
         GridPane.setColumnIndex(score2Pane, 2);
         score2Pane.setPrefHeight(88.0);
         score2Pane.setPrefWidth(142.0);
-        score2Pane.setStyle("-fx-background-radius: 15; -fx-background-color: blue;");
+        score2Pane.setStyle("-fx-background-radius: 15; -fx-background-color: white;");
 
         BorderPane.setAlignment(player2ScoreText, javafx.geometry.Pos.CENTER);
         player2ScoreText.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
@@ -303,10 +334,11 @@ public class GameBoardBase2Players extends AnchorPane implements ActionListener{
         player2Text.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
         player2Text.setStrokeWidth(0.0);
         player2Text.setText("Player 2");
-        player2Text.setFont(new Font("Arial Bold", 18.0));
+        player2Text.setFont(new Font("Arial Bold", 27.0));
         BorderPane.setMargin(player2Text, new Insets(5.0, 0.0, 0.0, 0.0));
         score2Pane.setTop(player2Text);
-
+        
+        
         newGameButton.setLayoutX(352.0);
         newGameButton.setLayoutY(552.0);
         newGameButton.setMnemonicParsing(false);
@@ -316,8 +348,15 @@ public class GameBoardBase2Players extends AnchorPane implements ActionListener{
         newGameButton.setText("New Game");
         newGameButton.setFont(new Font("Arial Bold", 14.0));
         newGameButton.setOpaqueInsets(new Insets(0.0));
-        newGameButton.setOnAction(new EventHandler<javafx.event.ActionEvent>() {
-            
+        newGameButton.setStyle("-fx-background-color: #68CFD1 ;"); 
+        newGameButton.setOnMouseEntered(event -> {
+            newGameButton.setStyle("-fx-background-color: #00CBFE;");
+        });
+
+        newGameButton.setOnMouseExited(event -> {
+            newGameButton.setStyle("-fx-background-color: #68CFD1 ;"); 
+        });
+        newGameButton.setOnAction(new EventHandler<javafx.event.ActionEvent>() { 
         @Override
         public void handle(javafx.event.ActionEvent event) {
            
@@ -325,8 +364,6 @@ public class GameBoardBase2Players extends AnchorPane implements ActionListener{
             buttonArray[i].setDisable(false);
             mouseClickCounter=0;
         }
-        
-        
         for(int i=0; i<9; i++){
             buttonArray[i].setStyle("-fx-background-color: #d7049e; -fx-text-fill: white;");
         }
@@ -350,16 +387,25 @@ public class GameBoardBase2Players extends AnchorPane implements ActionListener{
         );
         
         
-        exitButton.setLayoutX(528.0);
-        exitButton.setLayoutY(552.0);
-        exitButton.setMnemonicParsing(false);
-        exitButton.setPrefHeight(60.0);
-        exitButton.setPrefWidth(130.0);
-        exitButton.setStyle("-fx-background-radius: 15;");
-        exitButton.setText("Main Menu");
-        exitButton.setFont(new Font("Arial Bold", 14.0));
+        mainMenuButton.setLayoutX(528.0);
+        mainMenuButton.setLayoutY(552.0);
+        mainMenuButton.setMnemonicParsing(false);
+        mainMenuButton.setPrefHeight(60.0);
+        mainMenuButton.setPrefWidth(130.0);
+        mainMenuButton.setStyle("-fx-background-radius: 15;");
+        mainMenuButton.setText("Main Menu");
+        mainMenuButton.setFont(new Font("Arial Bold", 14.0));
+        mainMenuButton.setStyle("-fx-background-color: #68CFD1 ;");
         
-        exitButton.setOnAction(new EventHandler<javafx.event.ActionEvent>() {
+        mainMenuButton.setOnMouseEntered(event -> {
+            mainMenuButton.setStyle("-fx-background-color: #00CBFE;");
+        });
+
+        mainMenuButton.setOnMouseExited(event -> {
+            mainMenuButton.setStyle("-fx-background-color: #68CFD1 ;"); 
+        });
+        
+        mainMenuButton.setOnAction(new EventHandler<javafx.event.ActionEvent>() {
             @Override
             public void handle(javafx.event.ActionEvent event) {
                System.exit(0); 
@@ -368,12 +414,12 @@ public class GameBoardBase2Players extends AnchorPane implements ActionListener{
         );
         
 
-        playerTurnText.setLayoutX(760.0);
-        playerTurnText.setLayoutY(377.0);
+        playerTurnText.setLayoutX(770.0);
+        playerTurnText.setLayoutY(345.0);
         playerTurnText.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
-        playerTurnText.setStrokeWidth(0.0);
+        playerTurnText.setStrokeWidth(2.0);
         playerTurnText.setText("Player's Turn");
-        playerTurnText.setFont(new Font(36.0));
+        playerTurnText.setFont(new Font(70.0));
 
         getChildren().add(backgroundImage);
         boardGrid.getColumnConstraints().add(columnConstraints);
@@ -401,7 +447,7 @@ public class GameBoardBase2Players extends AnchorPane implements ActionListener{
         scoreGrid.getChildren().add(score2Pane);
         getChildren().add(scoreGrid);
         getChildren().add(newGameButton);
-        getChildren().add(exitButton);
+        getChildren().add(mainMenuButton);
         getChildren().add(playerTurnText);
 
         if(newGameFlag==true){
@@ -519,11 +565,11 @@ public class GameBoardBase2Players extends AnchorPane implements ActionListener{
                    if(buttonArray[i].getText()==""){
                        buttonArray[i].setTextFill(Color.rgb(197,160,215));
                        playerTurnText.setFill(javafx.scene.paint.Color.valueOf("#d7049e"));
-                       playerTurnText.setStroke(javafx.scene.paint.Color.valueOf("#d7049e"));
+                       playerTurnText.setStroke(javafx.scene.paint.Color.WHITE);
                        playerTurnText.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
                        buttonArray[i].setText("X");
                        player1Turn=false;
-                       playerTurnText.setText("O turn");
+                       playerTurnText.setText("O Turn");
                        checkIfAnyPlayerWon();
                    }
                }else{
@@ -531,10 +577,10 @@ public class GameBoardBase2Players extends AnchorPane implements ActionListener{
                        buttonArray[i].setTextFill(Color.rgb(197,160,215));
                        buttonArray[i].setText("O");
                        playerTurnText.setFill(javafx.scene.paint.Color.valueOf("#d7049e"));
-                       playerTurnText.setStroke(javafx.scene.paint.Color.valueOf("#d7049e"));
+                       playerTurnText.setStroke(javafx.scene.paint.Color.WHITE);
                        playerTurnText.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
                        player1Turn=true;
-                       playerTurnText.setText("X turn");   
+                       playerTurnText.setText("X Turn");   
                        checkIfAnyPlayerWon();
                    }   
                }
@@ -544,17 +590,21 @@ public class GameBoardBase2Players extends AnchorPane implements ActionListener{
     
     public void firstTurn(){
         if(random.nextInt(2)==0){
-            playerTurnText.setFill(javafx.scene.paint.Color.valueOf("#d7049e"));
+            playerTurnText.setFill(javafx.scene.paint.Color.WHITE);
             playerTurnText.setStroke(javafx.scene.paint.Color.valueOf("#d7049e"));
             playerTurnText.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
-            playerTurnText.setText("X turn");
+            playerTurnText.setStrokeWidth(5.0);
+            setEditsToPlayerTurnText();
+            playerTurnText.setText("X Turn");
             player1Turn=true;
 
         }else{  
-            playerTurnText.setFill(javafx.scene.paint.Color.valueOf("#d7049e"));
+            playerTurnText.setFill(javafx.scene.paint.Color.WHITE);
             playerTurnText.setStroke(javafx.scene.paint.Color.valueOf("#d7049e"));
             playerTurnText.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
-            playerTurnText.setText("O turn");
+            playerTurnText.setStrokeWidth(5.0);
+            setEditsToPlayerTurnText();
+            playerTurnText.setText("O Turn");
             player1Turn=false;
         }
     }
@@ -662,6 +712,8 @@ public class GameBoardBase2Players extends AnchorPane implements ActionListener{
                     x=x+1;
                     drawScoreText.setText(Integer.toString(x)); 
                     playerTurnText.setText("Draw");
+                    setEditsToPlayerTurnText();
+                    
                 }
 
             }
@@ -674,18 +726,17 @@ public class GameBoardBase2Players extends AnchorPane implements ActionListener{
         indexB=b;
         indexC=c;
         
-        buttonArray[a].setStyle("-fx-background-color: #68CFD1; -fx-text-fill: white;");
-        buttonArray[b].setStyle("-fx-background-color: #68CFD1; -fx-text-fill: white;");
-        buttonArray[c].setStyle("-fx-background-color: #68CFD1; -fx-text-fill: white;");
+        buttonArray[a].setStyle("-fx-background-color: #d7049e; -fx-text-fill: white;");
+        buttonArray[b].setStyle("-fx-background-color: #d7049e; -fx-text-fill: white;");
+        buttonArray[c].setStyle("-fx-background-color: #d7049e; -fx-text-fill: white;");
 
         for(int i=0; i<9; i++){
             if(buttonArray[i] != buttonArray[indexA] && buttonArray[i] != buttonArray[indexB] && buttonArray[i] != buttonArray[indexC] ){
                  buttonArray[i].setDisable(true);
             }
         }
-        
-     
         playerTurnText.setText("X Wins");
+        setEditsToPlayerTurnText();
         xWinCounter=xWinCounter+1;
         player1ScoreText.setText(Integer.toString(xWinCounter));
 
@@ -697,9 +748,9 @@ public class GameBoardBase2Players extends AnchorPane implements ActionListener{
         indexB=b;
         indexC=c;
         
-        buttonArray[a].setStyle("-fx-background-color: #68CFD1; -fx-text-fill: white;");
-        buttonArray[b].setStyle("-fx-background-color: #68CFD1; -fx-text-fill: white;");
-        buttonArray[c].setStyle("-fx-background-color: #68CFD1; -fx-text-fill: white;");
+        buttonArray[a].setStyle("-fx-background-color: #d7049e; -fx-text-fill: white;");
+        buttonArray[b].setStyle("-fx-background-color: #d7049e; -fx-text-fill: white;");
+        buttonArray[c].setStyle("-fx-background-color: #d7049e; -fx-text-fill: white;");
 
         for(int i=0; i<9; i++){
             if(buttonArray[i] != buttonArray[indexA] && buttonArray[i] != buttonArray[indexB] && buttonArray[i] != buttonArray[indexC] ){
@@ -708,10 +759,40 @@ public class GameBoardBase2Players extends AnchorPane implements ActionListener{
         }    
 
         playerTurnText.setText("O Wins"); 
+        setEditsToPlayerTurnText();
         player1Turn=false;
         oWinCounter=oWinCounter+1;
         player2ScoreText.setText(Integer.toString(oWinCounter)); 
         
     }
 
+    public void setEditsToPlayerTurnText(){
+                    InnerShadow innerShadow = new InnerShadow();
+                    innerShadow.setOffsetX(3.0);
+                    innerShadow.setOffsetY(2.0);
+                    innerShadow.setColor(Color.WHITE);
+                    playerTurnText.setEffect(innerShadow);
+
+                    Reflection reflection = new Reflection();
+                    reflection.setFraction(0.7);
+                    playerTurnText.setEffect(reflection);
+
+
+                    TranslateTransition textTranslateTransition = new TranslateTransition(Duration.seconds(1), playerTurnText);
+                    textTranslateTransition.setFromY(0);   // Set the starting Y position
+                    textTranslateTransition.setToY(50);   // Set the ending Y position
+                    textTranslateTransition.setAutoReverse(true);
+                    textTranslateTransition.setCycleCount(TranslateTransition.INDEFINITE); // Make it repeat indefinitely
+                    textTranslateTransition.play();
+
+                    // Apply 3D transformations
+                    PerspectiveCamera camera = new PerspectiveCamera(true);
+                    RotateTransition rotateTransition = new RotateTransition(Duration.seconds(3), playerTurnText);
+                    rotateTransition.setAxis(Rotate.Y_AXIS);
+                    rotateTransition.setByAngle(360);
+                    rotateTransition.setCycleCount(RotateTransition.INDEFINITE);
+                    rotateTransition.play();
+    }
+    
+    
 }
